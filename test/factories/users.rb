d97efedae7702cbe9a2256daadcb158cd4bd8801
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 20:34:45 +0200

    Added boolean is_admin to User table. Updated schema. Updated factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :user do
     email { "MyString" }
     password_digest { "MyString" }
+    is_admin { false }
+    factory :admin do
+      is_admin { true }
+    end
   end
+
 end
