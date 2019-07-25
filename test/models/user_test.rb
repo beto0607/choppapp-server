f739949b65b7de03a5182b89494254a6468c72b8
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 17:15:33 +0200

    Added test to User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should be valid" do
+    assert build(:user).valid?
+  end
+
+  test "Status should be ACTIVE when created" do
+    assert_equal create(:user).status, "ACTIVE"
+  end
+  
+  test "Should be invalid duplicated email" do
+    original = create(:user)
+    assert build(:user, email: original.email).invalid?
+  end
+
+  test "Should be invalid without email" do
+    assert build(:user, email: nil).invalid?
+  end
+
+  test "Should be invalid without password" do
+    assert build(:user, password_digest: nil).invalid?
+  end
 end
