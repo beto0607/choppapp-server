c776923066bc5082c1924a19fd0c3906c6b5d5a9
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 17:00:59 +0200

    Added Tests to Beer model

--- test/models/beer_test.rb
+++ test/models/beer_test.rb
@@ -1,7 +1,33 @@
-require 'test_helper'
+require "test_helper"
 
 class BeerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should be valid" do
+    assert build(:beer).valid?
+  end
+
+  test "Should be invalid without name" do
+    b = build(:beer, name: nil)
+    assert b.invalid?
+  end
+
+  test "Should be invalid without description" do
+    b = build(:beer, description: nil)
+    assert b.invalid?
+  end
+
+  test "Should be invalid without producer" do
+    b = build(:beer, producer: nil)
+    assert b.invalid?
+  end
+
+  test "Should be invalid without beer type" do
+    b = build(:beer, beer_type: nil)
+    assert b.invalid?
+  end
+
+  test "Should be invalid producer it's blocked" do
+    p = create(:producer, status: 'BLOCKED')
+    b = build(:beer, producer: p)
+    assert b.invalid?
+  end
 end
