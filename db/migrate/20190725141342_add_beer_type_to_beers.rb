e73acd01599bbc2367e411903fbb70eb74e15111
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 16:29:50 +0200

    Added Passwod to ENV variable. Fixed migration. Ran db:migrate

--- db/migrate/20190725141342_add_beer_type_to_beers.rb
+++ db/migrate/20190725141342_add_beer_type_to_beers.rb
@@ -1,4 +1,4 @@
-class AddBeerTypesToBeers < ActiveRecord::Migration[5.2]
+class AddBeerTypeToBeers < ActiveRecord::Migration[5.2]
   def change
     add_reference :beers, :beer_types, foreign_key: true
   end
