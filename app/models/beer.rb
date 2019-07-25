2c7997677d837c67eaa691ddfcba890fd6b69cb7
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 16:04:28 +0200

    Added validations and setted default value on creation to Beers

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,12 @@
 class Beer < ApplicationRecord
-  belongs_to :producer
+  belongs_to :producer, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :description, presence: true
+
+  has_many :votes, dependent: :destroy
+  
+  before_create do #Sets status to "ACTIVE" as default
+    self.status = "ACTIVE"
+  end
 end
