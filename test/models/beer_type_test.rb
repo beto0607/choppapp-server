948b57f475b01ea3a9a87f6347ef8a6e6fdd6153
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 17:05:38 +0200

    Added BeerType model tests

--- test/models/beer_type_test.rb
+++ test/models/beer_type_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class BeerTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should be valid" do
+    assert build(:beer_type).valid?
+  end
+
+  test "Should be invalid without name" do
+    bt = build(:beer_type, name: nil)
+    assert bt.invalid?
+  end
+
+  test "Should be invalid without description" do
+    bt = build(:beer_type, description: nil)
+    assert bt.invalid?
+  end
+
+  test "Should be invalid duplication" do
+    original = create(:beer_type)
+    bt = build(:beer_type, name: original.name)
+    assert bt.invalid?
+  end
 end
