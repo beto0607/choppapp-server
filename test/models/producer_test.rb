a61f6e5f07a60deb5fb2d33513da2be948d02ba4
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 17:10:50 +0200

    Added test to Producer model

--- test/models/producer_test.rb
+++ test/models/producer_test.rb
@@ -1,7 +1,33 @@
-require 'test_helper'
+require "test_helper"
 
 class ProducerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should be valid" do
+    assert build(:producer).valid?
+  end
+
+  test "Should be invalid when user's status it's BLOCKED" do
+    user = create(:user, status: "BLOCKED")
+    assert build(:producer, user: user).invalid?
+  end
+
+  test "Status should be PENDING when created" do
+    assert_equal create(:producer).status, "PENDING"
+  end
+
+  test "Should be invalid without user" do
+    assert build(:producer, user: nil).invalid?
+  end
+
+  test "Should be invalid without name" do
+    assert build(:producer, name: nil).invalid?
+  end
+
+  test "Should be invalid without description" do
+    assert build(:producer, description: nil).invalid?
+  end
+
+  test "Should be invalid duplicated name" do
+    original = create(:producer)
+    assert build(:producer, name: original.name).invalid?
+  end
 end
