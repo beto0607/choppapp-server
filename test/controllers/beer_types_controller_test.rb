e4da166472bd5a688c6c981cbf13f281ec06e4a3
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 20:26:29 +0200

    Added tests to Beer Type Controller

--- test/controllers/beer_types_controller_test.rb
+++ test/controllers/beer_types_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class BeerTypesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @beer_type = beer_types(:one)
+    @user = create(:user, is_admin: true)
+    @auth_header = { Authorization: "Bearer #{JsonWebToken.encode(user_id: @user.id)}" }
+    @beer_type = create(:beer_type)
   end
 
   test "should get index" do
@@ -12,9 +14,8 @@ class BeerTypesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create beer_type" do
     assert_difference('BeerType.count') do
-      post beer_types_url, params: { beer_type: { description: @beer_type.description, name: @beer_type.name } }, as: :json
+      post beer_types_url, params: { beer_type: attributes_for(:beer_type) }, as: :json, headers: @auth_header
     end
-
     assert_response 201
   end
 
@@ -23,16 +24,16 @@ class BeerTypesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should update beer_type" do
-    patch beer_type_url(@beer_type), params: { beer_type: { description: @beer_type.description, name: @beer_type.name } }, as: :json
-    assert_response 200
+  test "should get all beer from a beer_type" do
+    get beer_type_url(@beer_type), as: :json
+    assert_response :success
+    assert_equal @beer_type.beers.count, response.parsed_body.beers.count
   end
 
   test "should destroy beer_type" do
     assert_difference('BeerType.count', -1) do
-      delete beer_type_url(@beer_type), as: :json
+      delete beer_type_url(@beer_type), as: :json, headers: @auth_header
     end
-
     assert_response 204
   end
 end
