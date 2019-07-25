6c3da5e646437c0070502716a65a2638c6a5dcf1
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 20:59:52 +0200

    Added test to Beer type controller

--- test/controllers/beer_types_controller_test.rb
+++ test/controllers/beer_types_controller_test.rb
@@ -1,39 +1,32 @@
-require 'test_helper'
+require "test_helper"
 
 class BeerTypesControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = create(:user, is_admin: true)
-    @auth_header = { Authorization: "Bearer #{JsonWebToken.encode(user_id: @user.id)}" }
-    @beer_type = create(:beer_type)
-  end
-
+  # Index
   test "should get index" do
     get beer_types_url, as: :json
     assert_response :success
   end
-
-  test "should create beer_type" do
-    assert_difference('BeerType.count') do
-      post beer_types_url, params: { beer_type: attributes_for(:beer_type) }, as: :json, headers: @auth_header
-    end
-    assert_response 201
-  end
-
-  test "should show beer_type" do
-    get beer_type_url(@beer_type), as: :json
-    assert_response :success
-  end
-
   test "should get all beer from a beer_type" do
+    @beer_type = create(:beer_type)
+    @beer = create(:beer, beer_type: @beer_type)
     get beer_type_url(@beer_type), as: :json
     assert_response :success
     assert_equal @beer_type.beers.count, response.parsed_body.beers.count
   end
 
+  # Creation
+  test "should create beer_type" do
+    createAdmin()
+    post beer_types_url, params: { beer_type: attributes_for(:beer_type) }, as: :json, headers: @auth_header
+    assert_response 201
+  end
+
+  # Delete
   test "should destroy beer_type" do
-    assert_difference('BeerType.count', -1) do
-      delete beer_type_url(@beer_type), as: :json, headers: @auth_header
-    end
-    assert_response 204
+    createAdmin()
+    @beer_type = create(:beer_type)
+    delete beer_type_url(@beer_type), as: :json, headers: @auth_header
+    assert_response :no_content
+    assert @beer_type.destroyed?
   end
 end
