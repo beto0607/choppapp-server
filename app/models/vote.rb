bc93f568a8259ddbca6036e0fe8af371020620c2
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 16:11:08 +0200

    Added validations and remove dependant to Vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :beer
+
+  validates :score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+
 end
