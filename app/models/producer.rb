57752dbaf19f2981cbc8222ce19c40345d7d0b48
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 15:57:17 +0200

    Updated model for producers. Added reference to User

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,2 +1,7 @@
 class Producer < ApplicationRecord
+    validates :name, uniqueness: true, presense: true
+    validates :description, presense: true
+
+    belongs_to :user, dependent: :destroy
+    has_many :beers, dependent: :destroy
 end
