5cb6eb2b83cb80d3a788235ec7fd244463bb53d4
Roberto Albanesi <beto0607@gmail.comn>
2019-07-25 15:53:42 +0200

    Added scaffold of producers

---
+++ db/migrate/20190725135323_create_producers.rb
@@ -0,0 +1,12 @@
+class CreateProducers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :producers do |t|
+      t.string :name
+      t.text :description
+      t.string :place
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/factories/producers.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :producer do
+    name { "MyString" }
+    description { "MyText" }
+    place { "MyString" }
+    status { "MyString" }
+  end
+end
