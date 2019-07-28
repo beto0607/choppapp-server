5980aefd7de8f165494a63f67a898ffe564036b2
Roberto Albanesi <beto0607@gmail.comn>
2019-07-28 21:20:35 +0200

    Added test to user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,38 +1,95 @@
-require 'test_helper'
+require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @user = users(:one)
-  end
-
+  # Index
   test "should get index" do
+    create_admin()
+    get users_url, as: :json, headers: @auth_header
+    assert_response :ok
+  end
+  test "shouldn't get index if user isn't a admin" do
+    create_auth_header()
+    get users_url, as: :json, headers: @auth_header
+    assert_response :unauthorized
+  end
+  test "shouldn't get index if user isn't logged in" do
     get users_url, as: :json
-    assert_response :success
+    assert_response :unauthorized
   end
 
-  test "should create user" do
-    assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, password_digest: @user.password_digest } }, as: :json
-    end
-
-    assert_response 201
-  end
-
+  # Show
   test "should show user" do
+    create_user()
     get user_url(@user), as: :json
-    assert_response :success
+    assert_response :ok
+  end
+  test "should return 404 - show" do
+    get user_url(-1), as: :json
+    assert_response :not_found
+  end
+  test "shouldn't show info of a private user" do
+    skip()
+    create_user()
+    @user.update(private: true)
+    get user_url(@user), as: :json
+    assert_response :not_found
   end
 
-  test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, password_digest: @user.password_digest } }, as: :json
-    assert_response 200
+  # Active
+  test "should active user" do
+    skip()
+    @pending_user = create(:user)
+    create_admin()
+    patch user_url(@pending_user) + "/active", as: :json, headers: @auth_header
+    assert_response :ok
+  end
+  test "Logged user must be admin - Active" do
+    skip()
+    @pending_user = create(:user)
+    create_auth_header() #Not admin
+    patch user_url(@pending_user) + "/active", as: :json, headers: @auth_header
+    assert_response :unauthorized
   end
 
+  # Block
+  test "should block user" do
+    skip()
+    @pending_user = create(:user)
+    create_admin()
+    patch user_url(@pending_user) + "/block", as: :json, headers: @auth_header
+    assert_response :ok
+  end
+  test "Logged user must be admin - Block" do
+    skip()
+    @pending_user = create(:user)
+    create_auth_header() #Not admin
+    patch user_url(@pending_user) + "/block", as: :json, headers: @auth_header
+    assert_response :unauthorized
+  end
+
+  # Delete
   test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user), as: :json
-    end
-
-    assert_response 204
+    create_auth_header()
+    delete user_url(@user), as: :json, headers: @auth_header
+    assert_response :no_content
+    assert @new_user.destroyed?
+  end
+  test "should destroy user by admin" do
+    create_admin()
+    @new_user = create(:user)
+    delete user_url(@new_user), as: :json, headers: @auth_header
+    assert_response :no_content
+    assert @new_user.destroyed?
+  end
+  test "should return 404 - delete" do
+    create_auth_header()
+    delete user_url(-1), as: :json, headers: @auth_header
+    assert_response :not_found
+  end
+  test "user logged in must be the user to delete or admin" do
+    create_auth_header()
+    @new_user = create(:user)
+    delete user_url(@new_user), as: :json, headers: @auth_header
+    assert_response :unauthorized
   end
 end
