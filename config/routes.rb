d99f254d13613f45707b7356ba4007ca3b8ee212
Roberto Albanesi <beto0607@gmail.comn>
2019-07-29 14:00:00 +0200

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :beer_types
+  resources :beer_types, only: [:index, :create, :delete] do
+    get :beers, to: "beers#by_beer_type"
+  end
+  resources :beers do
+    get :votes, to: "votes#by_beer"
+  end
+  resources :producers do
+    get :beers, to: "beers#by_producer"
+    get :votes, to: "votes#by_producer"
+  end
   resources :votes
-  resources :beers
-  resources :producers
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only:[:index, :show, :delete ]do 
+    match 'block', to: 'user#block', via: [:put, :patch]
+    match 'active', to: 'user#active', via: [:put, :patch]
+  end
 end
